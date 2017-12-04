47e45e9cfefeb7fbb710cabee3e4e9fe37c9504e
Thomas Thimothee <thom.thimothee@gmail.com>
2017-12-04 11:02:15 +0100

    .

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -25,24 +25,24 @@ import org.junit.Test;
  * @author mathiasjepsen
  */
 public class Tests {
-    
+
     Home home1;
     Address address1;
     Role adminRole;
     Role userRole;
     User user1;
-    
+
     public Tests() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() throws WrongPhoneNumberFormatException, WrongZipNumberFormatException, WrongCityFormatException {
         address1 = new Address("Holte", "2840", "Kongevejen 438");
@@ -51,11 +51,11 @@ public class Tests {
         userRole = new Role("User");
         user1 = new User("lovro", "1234", "Lovro", "Billyjessica", "432543534", "lovro@mail.com");
     }
-    
+
     @After
     public void tearDown() {
     }
-    
+
     @Test
     public void calculateAverageRatingTest() {
         HashMap<String, Double> ratings = new HashMap();
@@ -67,7 +67,7 @@ public class Tests {
         int calculatedRating = home1.calculateRating();
         assertEquals(3, calculatedRating);
     }
-    
+
     @Test
     public void addHomeBookingTest() {
         HomeBooking booking1 = new HomeBooking("Lovro", "03-12-2017");
@@ -78,55 +78,65 @@ public class Tests {
         home1.addBooking(booking3);
         assertEquals(3, home1.getHomeBookings().size());
     }
+
     @Test(expected = WrongPhoneNumberFormatException.class)
     public void WrongFormatPhoneTest() throws WrongPhoneNumberFormatException {
-      User userTest = new User("lovro", "1234", "Lovro", "Billyjessica", "blabla", "lovro@mail.com");
+        User userTest = new User("lovro", "1234", "Lovro", "Billyjessica", "blabla", "lovro@mail.com");
     }
-     @Test(expected = WrongPhoneNumberFormatException.class)
+
+    @Test(expected = WrongPhoneNumberFormatException.class)
     public void WrongFormatPhoneTestEmptyString() throws WrongPhoneNumberFormatException {
-      User userTest = new User("lovro", "1234", "Lovro", "Billyjessica", "", "lovro@mail.com");
+        User userTest = new User("lovro", "1234", "Lovro", "Billyjessica", "", "lovro@mail.com");
     }
-     @Test(expected = WrongPhoneNumberFormatException.class)
+
+    @Test(expected = WrongPhoneNumberFormatException.class)
     public void WrongFormatPhoneTestNumberAndLetters() throws WrongPhoneNumberFormatException {
-      User userTest = new User("lovro", "1234", "Lovro", "Billyjessica", "123b456", "lovro@mail.com");
+        User userTest = new User("lovro", "1234", "Lovro", "Billyjessica", "123b456", "lovro@mail.com");
     }
-     @Test(expected = WrongPhoneNumberFormatException.class)
+
+    @Test(expected = WrongPhoneNumberFormatException.class)
     public void WrongFormatPhoneTestSpecialCharacters() throws WrongPhoneNumberFormatException {
-      User userTest = new User("lovro", "1234", "Lovro", "Billyjessica", "123/456", "lovro@mail.com");
+        User userTest = new User("lovro", "1234", "Lovro", "Billyjessica", "123/456", "lovro@mail.com");
     }
-     @Test(expected = WrongPhoneNumberFormatException.class)
+
+    @Test(expected = WrongPhoneNumberFormatException.class)
     public void WrongFormatPhoneTestSpace() throws WrongPhoneNumberFormatException {
-      User userTest = new User("lovro", "1234", "Lovro", "Billyjessica", " ", "lovro@mail.com");
+        User userTest = new User("lovro", "1234", "Lovro", "Billyjessica", " ", "lovro@mail.com");
     }
-     @Test(expected = WrongPhoneNumberFormatException.class)
+
+    @Test(expected = WrongPhoneNumberFormatException.class)
     public void WrongFormatPhoneTestNumbersAndSpace() throws WrongPhoneNumberFormatException {
-      User userTest = new User("lovro", "1234", "Lovro", "Billyjessica", "678 989", "lovro@mail.com");
+        User userTest = new User("lovro", "1234", "Lovro", "Billyjessica", "678 989", "lovro@mail.com");
     }
-    @Test(expected = WrongZipNumberFormatException.class) 
+
+    @Test(expected = WrongZipNumberFormatException.class)
     public void WrongFormatZipTest() throws WrongZipNumberFormatException, WrongCityFormatException {
         Address addressTest = new Address("Copenhagen", "testing", "Gl. Amtsvej");
     }
-    @Test(expected = WrongCityFormatException.class) 
-    public void WrongFormatCityTest() throws WrongCityFormatException, WrongZipNumberFormatException {
-    Address addresTest2 = new Address("471207", "1620", "Vestebrogade");
-    }
-     @Test(expected = WrongCityFormatException.class) 
-    public void WrongFormatCityTestEmptyString() throws WrongCityFormatException, WrongZipNumberFormatException {
-    Address addresTest2 = new Address("", "1620", "Vestebrogade");
-    }
-     @Test(expected = WrongCityFormatException.class) 
-    public void WrongFormatCityTestSpecialCharacter() throws WrongCityFormatException, WrongZipNumberFormatException {
-    Address addresTest2 = new Address("/*", "1620", "Vestebrogade");
-    }
-     @Test(expected = WrongCityFormatException.class) 
-    public void WrongFormatCityTestNumbersAndSpace() throws WrongCityFormatException, WrongZipNumberFormatException {
-    Address addresTest2 = new Address("471 97", "1620", "Vestebrogade");
-    }
-     @Test(expected = WrongCityFormatException.class) 
-    public void WrongFormatCityTestSpace() throws WrongCityFormatException, WrongZipNumberFormatException {
-    Address addresTest2 = new Address(" ", "1620", "Vestebrogade");
-    }
-    
 
-    
+    @Test(expected = WrongCityFormatException.class)
+    public void WrongFormatCityTest() throws WrongCityFormatException, WrongZipNumberFormatException {
+        Address addresTest = new Address("471207", "1620", "Vestebrogade");
+    }
+
+    @Test(expected = WrongCityFormatException.class)
+    public void WrongFormatCityTestEmptyString() throws WrongCityFormatException, WrongZipNumberFormatException {
+        Address addressTest = new Address("", "1620", "Vestebrogade");
+    }
+
+    @Test(expected = WrongCityFormatException.class)
+    public void WrongFormatCityTestSpecialCharacter() throws WrongCityFormatException, WrongZipNumberFormatException {
+        Address addressTest = new Address("/*", "1620", "Vestebrogade");
+    }
+
+    @Test(expected = WrongCityFormatException.class)
+    public void WrongFormatCityTestNumbersAndSpace() throws WrongCityFormatException, WrongZipNumberFormatException {
+        Address addressTest = new Address("471 97", "1620", "Vestebrogade");
+    }
+
+    @Test(expected = WrongCityFormatException.class)
+    public void WrongFormatCityTestSpace() throws WrongCityFormatException, WrongZipNumberFormatException {
+        Address addressTest = new Address(" ", "1620", "Vestebrogade");
+    }
+
 }
